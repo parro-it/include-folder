a34488664cd4fa32a63176f82e56f29ed5cf7ddf
Andrea Parodi <andrea.parodi@ebansoftware.net>
2014-01-18 11:02:45 +0100

    handle of hidden files

--- test/include-folder_test.js
+++ test/include-folder_test.js
@@ -5,51 +5,76 @@ var expect = require("expect.js"),
     includeFolder = require("../lib/include-folder");
 
 
-describe("include_folder", function () {
-    it("is defined", function () {
+describe("normalize", function() {
+    it("handles .DS_Store",function() {
+           var normalized = includeFolder._testHook.normalize(".DS_Store");
+            expect(normalized).to.be.equal("DS_Store");        
+    });
+});
+
+describe("stripExtension", function() {
+    it("remove extension",function() {
+           var normalized = includeFolder._testHook.stripExtension("test.txt");
+            expect(normalized).to.be.equal("test");        
+    });
+
+    it("handles hidden files",function() {
+           var normalized = includeFolder._testHook.stripExtension(".txt");
+            expect(normalized).to.be.equal("txt");        
+    });
+});
+
+
+
+describe("include_folder", function() {
+    it("is defined", function() {
         expect(includeFolder).to.be.an('function');
     });
 
-    it("has test hook", function () {
+    it("has test hook", function() {
         expect(includeFolder._testHook).to.be.an('object');
     });
 
     function moduleCheck(folderModule) {
-        it("is an object", function () {
+        it("is an object", function() {
             expect(folderModule).to.be.an('object');
         });
 
 
-        it("has a property for each file", function () {
-            expect(_.keys(folderModule).length).to.be.equal(3);
+        it("has a property for each file", function() {
+            expect(_.keys(folderModule).length).to.be.equal(4);
         });
 
-        it("extension is stripped from file names", function () {
+        it("extension is stripped from file names", function() {
             expect("file1" in folderModule).to.be.equal(true);
         });
 
-        it("duplicated file names properties are incremented", function () {
+        it("duplicated file names properties are incremented", function() {
             expect("file1_1" in folderModule).to.be.equal(true);
         });
 
-        it("properties names are normalized", function () {
+        it("properties names are normalized", function() {
             expect("file3OtherFile" in folderModule).to.be.equal(true);
         });
 
-        it("properties values are file content", function () {
+        it("properties values are file content", function() {
             expect(folderModule.file1).to.be.equal("this is file1 content");
         });
     }
 
-    describe ("generated source code",function(){
-        var source = includeFolder._testHook.buildSource("./test/files"),
-            folderModule = (new Function("require","__dirname",source))(require,__dirname+"/files/");
+    describe("generated source code", function() {
+        var source,
+            folderModule;
 
+        source = includeFolder._testHook.buildSource("./test/files"),
+        console.log(source)
+        folderModule = (new Function("require", "__dirname", source))(require, __dirname + "/files/");
 
         moduleCheck(folderModule);
     });
 
-    describe ("returned object",function(){
+
+    describe("returned object", function() {
 
 
         var folderModule = includeFolder("./test/files");
