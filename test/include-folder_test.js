b2a0b5687c3eab3c5d49f908a02fa80be028fd9a
Andrea Parodi <andrea@parro.it>
2015-08-22 19:04:12 +0200

    switch to eslint

--- test/include-folder_test.js
+++ test/include-folder_test.js
@@ -1,101 +1,91 @@
 'use strict';
 
-/*jshint -W054 */
+var expect = require('expect.js');
+var includeFolder = require('../lib/include-folder');
 
-var expect = require('expect.js'),
-    includeFolder = require('../lib/include-folder');
-
-
-describe('normalize', function() {
-    it('handles .DS_Store', function() {
-        var normalized = includeFolder.normalize('.DS_Store');
-        expect(normalized).to.be.equal('DS_Store');
-    });
+describe('normalize', function () {
+  it('handles .DS_Store', function () {
+    var normalized = includeFolder.normalize('.DS_Store');
+    expect(normalized).to.be.equal('DS_Store');
+  });
 });
 
-describe('stripExtension', function() {
-    it('remove extension', function() {
-        var normalized = includeFolder.stripExtension('test.txt');
-        expect(normalized).to.be.equal('test');
-    });
+describe('stripExtension', function () {
+  it('remove extension', function () {
+    var normalized = includeFolder.stripExtension('test.txt');
+    expect(normalized).to.be.equal('test');
+  });
 
-    it('handles hidden files', function() {
-        var normalized = includeFolder.stripExtension('.txt');
-        expect(normalized).to.be.equal('txt');
-    });
+  it('handles hidden files', function () {
+    var normalized = includeFolder.stripExtension('.txt');
+    expect(normalized).to.be.equal('txt');
+  });
 });
 
+describe('include_folder', function () {
+  it('is defined', function () {
+    expect(includeFolder).to.be.a('function');
+  });
 
-
-describe('include_folder', function() {
-    it('is defined', function() {
-        expect(includeFolder).to.be.a('function');
+  function moduleCheck (folderModule, expectedCount) {
+    it('is an object', function () {
+      expect(folderModule).to.be.an('object');
     });
 
-    function moduleCheck(folderModule, expectedCount) {
-        it('is an object', function() {
-            expect(folderModule).to.be.an('object');
-        });
-
-        it('has a property for each file', function() {
-            expect(Object.keys(folderModule).length).to.be.equal(expectedCount);
-        });
-
-        it('extension is stripped from file names', function() {
-            expect('file1' in folderModule).to.be.equal(true);
-        });
-
-        it('duplicated file names properties are incremented', function() {
-            expect('file1_1' in folderModule).to.be.equal(true);
-        });
-
-        it('properties names are normalized', function() {
-            expect('file3OtherFile' in folderModule).to.be.equal(true);
-        });
-
-        it('properties values are file content', function() {
-            expect(folderModule.file1).to.be.equal('this is file1 content');
-        });
-    }
-
-    describe('generated source code', function() {
-        var source,
-            folderModule,
-            folderFun;
-
-        source = includeFolder.buildSource('./test/files', /^[^.].*$/);
-        folderFun = new Function('require', '__dirname', source);
-        folderModule = folderFun(require, __dirname + '/files/');
-
-        moduleCheck(folderModule, 3);
+    it('has a property for each file', function () {
+      expect(Object.keys(folderModule).length).to.be.equal(expectedCount);
     });
 
-
-    describe('returned object', function() {
-        var folderModule = includeFolder('./test/files');
-
-        moduleCheck(folderModule, 3);
+    it('extension is stripped from file names', function () {
+      expect('file1' in folderModule).to.be.equal(true);
     });
 
-    describe('work with hidden files', function() {
-        var folderModule = includeFolder('./test/files', /.*/);
-
-        moduleCheck(folderModule, 4);
+    it('duplicated file names properties are incremented', function () {
+      expect('file1_1' in folderModule).to.be.equal(true);
     });
 
-    describe('when the preserveFilenames option is true', function() {
-        beforeEach(function(){
-            this.folderModule = includeFolder('./test/files', null, { preserveFilenames: true });
-        });
-
-        it('extension is not stripped from file names', function() {
-            expect('file1.txt' in this.folderModule).to.be.equal(true);
-            expect('file1.check' in this.folderModule).to.be.equal(true);
-        });
-
-        it('filename is not normalized', function() {
-            expect('file-3-other&file.txt' in this.folderModule).to.be.equal(true);
-        });
+    it('properties names are normalized', function () {
+      expect('file3OtherFile' in folderModule).to.be.equal(true);
     });
+
+    it('properties values are file content', function () {
+      expect(folderModule.file1).to.be.equal('this is file1 content');
+    });
+  }
+
+  describe('generated source code', function () {
+    var source = includeFolder.buildSource('./test/files', /^[^.].*$/);
+    var folderFun = new Function('require', '__dirname', source); // eslint-disable-line no-new-func
+    var folderModule = folderFun(require, __dirname + '/files/');
+
+    moduleCheck(folderModule, 3);
+  });
+
+  describe('returned object', function () {
+    var folderModule = includeFolder('./test/files');
+
+    moduleCheck(folderModule, 3);
+  });
+
+  describe('work with hidden files', function () {
+    var folderModule = includeFolder('./test/files', /.*/);
+
+    moduleCheck(folderModule, 4);
+  });
+
+  describe('when the preserveFilenames option is true', function () {
+    beforeEach(function () {
+      this.folderModule = includeFolder('./test/files', null, { preserveFilenames: true });
+    });
+
+    it('extension is not stripped from file names', function () {
+      expect('file1.txt' in this.folderModule).to.be.equal(true);
+      expect('file1.check' in this.folderModule).to.be.equal(true);
+    });
+
+    it('filename is not normalized', function () {
+      expect('file-3-other&file.txt' in this.folderModule).to.be.equal(true);
+    });
+  });
 
 });
